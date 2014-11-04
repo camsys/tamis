5f6aac1cd83c0e70a5f4bbb434e254b0d78e8794
dbenoff <dbenoff@camsys.com>
2014-11-04 09:47:59 -0800

    modify charts to graph multiple series

--- app/config/config.js
+++ app/config/config.js
@@ -4,7 +4,7 @@ define(
             regionQueryUrl: 'http://swdev34/ArcGIS/rest/services/AKDOT/test/MapServer/exts/RouteEventOverlaySOE/GetGeogAreaValues?AreaType=Region&f=pjson',
             districtQueryUrl: 'http://swdev34/ArcGIS/rest/services/AKDOT/test/MapServer/exts/RouteEventOverlaySOE/GetGeogAreaValues?AreaType=HouseDistrict&f=pjson',
             functionalClassQueryUrl: 'http://swdev34/ArcGIS/rest/services/AKDOT/test/MapServer/exts/RouteEventOverlaySOE/GetFilterValues?FilterType=FunctionalClass&f=pjson',
-            runQueryUrl: '/tamisPilot/web/Query.aspx/RunQuery',
+            runQueryUrl: '/tamisPilotQA/web/Query.aspx/RunQuery',
             noResultsMessage:{title: "Whoops!", message: "No data is available for display yet, run a query first."}
         };
     });
